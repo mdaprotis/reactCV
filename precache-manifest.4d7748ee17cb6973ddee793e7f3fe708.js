f8902ddba96a8f3cfe42fa5b43679f72d0e15cf0
mdaprotis <mdaprotis@hotmail.com>
2020-04-08 14:22:08 -0300

    Updates

---
+++ precache-manifest.4d7748ee17cb6973ddee793e7f3fe708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0470ff221e74bb71f1d10f38ba1fd8",
+    "url": "/react_cv/index.html"
+  },
+  {
+    "revision": "a1d1c65162fa2d173cf8",
+    "url": "/react_cv/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "a1d1c65162fa2d173cf8",
+    "url": "/react_cv/static/js/2.5d00b908.chunk.js"
+  },
+  {
+    "revision": "4727a15faee1ffb746140055acc50aa8",
+    "url": "/react_cv/static/js/2.5d00b908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03394bc75bf97d97dee0",
+    "url": "/react_cv/static/js/main.fb466453.chunk.js"
+  },
+  {
+    "revision": "3bb0746e303999a13a5a",
+    "url": "/react_cv/static/js/runtime-main.87b841ff.js"
+  },
+  {
+    "revision": "0fe21dfd48452094e2aec6366a3298d4",
+    "url": "/react_cv/static/media/pic.0fe21dfd.png"
+  }
+]);
