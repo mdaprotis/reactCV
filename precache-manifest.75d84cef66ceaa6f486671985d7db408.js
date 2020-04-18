c4a3655a7920ac926a9a5001dd5b73adb140b8b3
mdaprotis <mdaprotis@hotmail.com>
2020-04-18 18:29:30 -0300

    Updates

---
+++ precache-manifest.75d84cef66ceaa6f486671985d7db408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd59676d3ea12d655fef61b5cbc5e222",
+    "url": "/reactCV/index.html"
+  },
+  {
+    "revision": "4d0262d3255dfff1461f",
+    "url": "/reactCV/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "4d0262d3255dfff1461f",
+    "url": "/reactCV/static/js/2.86f20c92.chunk.js"
+  },
+  {
+    "revision": "4727a15faee1ffb746140055acc50aa8",
+    "url": "/reactCV/static/js/2.86f20c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8fe89af379f48b12bde",
+    "url": "/reactCV/static/js/main.75761362.chunk.js"
+  },
+  {
+    "revision": "d41fe63ce308c471c10b",
+    "url": "/reactCV/static/js/runtime-main.55135875.js"
+  },
+  {
+    "revision": "f374d503d667532510754e5820b2a90f",
+    "url": "/reactCV/static/media/pic4.f374d503.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_cv/precache-manifest.bd567040e57071e2a8e4b03cc1f20844.js"
+  "/reactCV/precache-manifest.75d84cef66ceaa6f486671985d7db408.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_cv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactCV/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
